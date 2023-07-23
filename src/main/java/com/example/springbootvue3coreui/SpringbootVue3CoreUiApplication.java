58a5723c67435018a96a6e1fc610be758f90d600
j-bae <j-bae@kakao.com>
2023-07-23 20:50:27 +0900

    [*] init

---
+++ src/main/java/com/example/springbootvue3coreui/SpringbootVue3CoreUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootvue3coreui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVue3CoreUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVue3CoreUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootvue3coreui/SpringbootVue3CoreUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootvue3coreui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVue3CoreUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
